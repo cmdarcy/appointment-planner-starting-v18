630d1e22e95735bf72987cb1bf9aecfc6c49a9e1
Christopher d'Arcy <cdarcy656481@gmail.com>
2024-04-12 00:25:18 -0400

    scaffold contactForm component in ContactForm.js

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,16 +1,40 @@
 import React from "react";
 
 export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
+	name,
+	setName,
+	phone,
+	setPhone,
+	email,
+	setEmail,
+	handleSubmit,
 }) => {
-  return (
-    <></>
-  );
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				></input>
+				<input
+					type="tel"
+					placeholder="Phone"
+					value={phone}
+					onChange={(e) => setPhone(e.target.value)}
+					pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+					maxLength="10"
+				></input>
+				<input
+					type="email"
+					placeholder="Email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+				></input>
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	);
 };
-
